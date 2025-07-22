c8d40342dcc883c8df311590b09ec00f44186def
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-07-21 20:00:13 -0500

    Creacion y configuracion del archivo urls.py de Editorial

---
+++ Aplicaciones/Editorial/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listaEditorial, name='lista_editorial'),
+    path('nuevo/', views.nuevoEditorial, name='nuevo_editorial'),
+    path('guardar/', views.guardarEditorial, name='guardar_editorial'),
+    path('<int:id>/editar/', views.editarTaller, name='editar_editorial'),
+    path('<int:id>/actualizar/', views.procesarEditorial, name='procesar_editorial'),
+    path('eliminar/<int:id>/', views.eliminarEditorial, name='eliminar_editorial'),
+]
