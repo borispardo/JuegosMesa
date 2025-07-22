0882c64bcb4a241183c5924210c958d9bce57ce4
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-07-21 20:48:21 -0500

    Configuracion de apps.py admin.py models.py y migraciones de Coleccionista

--- Aplicaciones/Coleccionista/models.py
+++ Aplicaciones/Coleccionista/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Coleccionista(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=100)
+    correo = models.EmailField()
+    fecha_registro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
--- JuegosMesa/settings.py
+++ JuegosMesa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Aplicaciones.Editorial', 
+    'Aplicaciones.Coleccionista',
 ]
 
 MIDDLEWARE = [
