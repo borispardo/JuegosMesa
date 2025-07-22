126c818ecd171f6bb5b4309a9d6a5418336c75b7
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-07-22 02:19:36 -0500

    Insercion de la plantilla profesional con sus respectivas adaptaciones

--- Aplicaciones/Juego/views.py
+++ Aplicaciones/Juego/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
 from .models import Juego
 from Aplicaciones.Coleccionista.models import Coleccionista
 from Aplicaciones.Editorial.models import Editorial
-from django.contrib import messages
 
 # Listar Juegos
 def listaJuego(request):
@@ -35,17 +36,17 @@ def guardarJuego(request):
             editorial_id=editorial_id
         )
 
-        messages.success(request, "Juego GUARDADO correctamente.")
-        return redirect('/juego/')
-    else:
-        return redirect('/nuevo/')
+        messages.success(request, "Juego guardado correctamente.")
+        return redirect(reverse('lista_juego'))
+    
+    return redirect(reverse('nuevo_juego'))
 
 # Formulario editar juego
 def editarJuego(request, id):
     juego = get_object_or_404(Juego, id=id)
     coleccionistas = Coleccionista.objects.all()
     editoriales = Editorial.objects.all()
-    return render(request, 'juego/editar.html', {
+    return render(request, 'Juego/editar.html', {
         'juego': juego,
         'coleccionistas': coleccionistas,
         'editoriales': editoriales
@@ -63,14 +64,14 @@ def procesarJuego(request, id):
         juego.editorial_id = request.POST.get("editorial")
         juego.save()
 
-        messages.success(request, "Juego EDITADO correctamente.")
-        return redirect('/juego/')
-    else:
-        return redirect('/juego/')
+        messages.success(request, "Juego editado correctamente.")
+        return redirect(reverse('lista_juego'))
+
+    return redirect(reverse('lista_juego'))
 
 # Eliminar juego
 def eliminarJuego(request, id):
     juego = get_object_or_404(Juego, id=id)
     juego.delete()
-    messages.success(request, "Juego ELIMINADO correctamente.")
-    return redirect('/juego/')
+    messages.success(request, "Juego eliminado correctamente.")
+    return redirect(reverse('lista_juego'))
